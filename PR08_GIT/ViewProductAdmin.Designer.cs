3cc1a4097fc970b7dffe906e214384e118533d0d
Dmitriy Chirkov <fyerwf@yandex.ru>
2025-10-17 12:55:49 +0300

    Задание 4. Дизайн формы

--- PR08_GIT/ViewProductAdmin.Designer.cs
+++ PR08_GIT/ViewProductAdmin.Designer.cs
@@ -29,6 +29,7 @@ namespace PR08_GIT
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ViewProductAdmin));
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
@@ -43,11 +44,14 @@ namespace PR08_GIT
             this.label4 = new System.Windows.Forms.Label();
             this.textBox5 = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.Chartreuse;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(12, 68);
             this.dataGridView1.Name = "dataGridView1";
@@ -56,34 +60,45 @@ namespace PR08_GIT
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.Color.Chartreuse;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.MediumSpringGreen;
             this.button1.Location = new System.Drawing.Point(12, 641);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(111, 43);
             this.button1.TabIndex = 1;
             this.button1.Text = "Добавить";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // button2
             // 
+            this.button2.BackColor = System.Drawing.Color.Chartreuse;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.Color.MediumSpringGreen;
             this.button2.Location = new System.Drawing.Point(129, 642);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(136, 43);
+            this.button2.Size = new System.Drawing.Size(148, 43);
             this.button2.TabIndex = 2;
             this.button2.Text = "Редактировать";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(271, 642);
+            this.button3.BackColor = System.Drawing.Color.Chartreuse;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.ForeColor = System.Drawing.Color.MediumSpringGreen;
+            this.button3.Location = new System.Drawing.Point(283, 641);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(111, 43);
             this.button3.TabIndex = 3;
             this.button3.Text = "Удалить";
-            this.button3.UseVisualStyleBackColor = true;
+            this.button3.UseVisualStyleBackColor = false;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.ForeColor = System.Drawing.Color.MediumSpringGreen;
             this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(130, 21);
@@ -107,6 +122,7 @@ namespace PR08_GIT
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.MediumSpringGreen;
             this.label2.Location = new System.Drawing.Point(171, 9);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(98, 21);
@@ -123,6 +139,7 @@ namespace PR08_GIT
             // label3
             // 
             this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.MediumSpringGreen;
             this.label3.Location = new System.Drawing.Point(330, 9);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(104, 21);
@@ -139,6 +156,7 @@ namespace PR08_GIT
             // label4
             // 
             this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.MediumSpringGreen;
             this.label4.Location = new System.Drawing.Point(489, 9);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(73, 21);
@@ -155,11 +173,22 @@ namespace PR08_GIT
             // label5
             // 
             this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.MediumSpringGreen;
             this.label5.Location = new System.Drawing.Point(648, 9);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(103, 21);
             this.label5.TabIndex = 12;
             this.label5.Text = "Стоимость:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(854, 9);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 14;
+            this.pictureBox1.TabStop = false;
             // 
             // ViewProductAdmin
             // 
@@ -167,6 +196,7 @@ namespace PR08_GIT
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(966, 697);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.textBox5);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.textBox4);
@@ -183,6 +213,7 @@ namespace PR08_GIT
             this.Controls.Add(this.dataGridView1);
             this.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
@@ -190,6 +221,7 @@ namespace PR08_GIT
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Просмотр товаров админ";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -211,5 +243,6 @@ namespace PR08_GIT
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox textBox5;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
